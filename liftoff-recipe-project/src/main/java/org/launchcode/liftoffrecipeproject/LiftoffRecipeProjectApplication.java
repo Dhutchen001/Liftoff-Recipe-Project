3e90c5e7e569ee18172460c8433921092450d7a7
Dhutchen001 <117687122+Dhutchen001@users.noreply.github.com>
2023-04-17 20:22:58 -0500

    Add files via upload

---
+++ liftoff-recipe-project/src/main/java/org/launchcode/liftoffrecipeproject/LiftoffRecipeProjectApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.liftoffrecipeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftoffRecipeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftoffRecipeProjectApplication.class, args);
+	}
+
+}
---
+++ liftoff-recipe-project/src/test/java/org/launchcode/liftoffrecipeproject/LiftoffRecipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.liftoffrecipeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftoffRecipeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
